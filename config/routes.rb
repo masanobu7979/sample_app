737f26c9a0050630e5e56138afb3328b252b398e
masanobuo <masanobu7879@gmail.com>
2020-02-19 03:56:00 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
- root 'static_pages#home'
+  get 'users/new'
+
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 end
+
+#ページ名 	URL 	名前付きルート
+#Home 	  / 	      root_path
+#About  	/about 	  about_path
+#Help 	  /help 	  help_path
+#Contact 	/contact 	contact_path
+#Sign up 	/signup 	signup_path
+#Log in 	/login 	  login_path
