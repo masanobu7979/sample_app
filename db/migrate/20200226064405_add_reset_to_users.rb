67dc7fd1d2f08ac8b8e7d064d36b222f4a401a2e
masanobuo <masanobu7879@gmail.com>
2020-03-01 23:05:53 +0000

    fix#2

--- db/migrate/20200226064405_add_reset_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddResetToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :reset_digest, :stringreset_sent_at
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200228122219) do
+ActiveRecord::Schema.define(version: 20200301230027) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -32,7 +32,20 @@ ActiveRecord::Schema.define(version: 20200228122219) do
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'stringreset_sent_at' for column 'reset_digest'
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 
 end
